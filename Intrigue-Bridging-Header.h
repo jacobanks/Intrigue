d25017433ef3765845432a945ba4468dab1c6a66
jacobanks <jacobanks@gmail.com>
2015-09-12 02:57:14 -0400

    Added bridging header and added a JSQMessagesViewController

---
+++ Intrigue-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Intrigue-Bridging-Header\.h
+//  Intrigue
+//
+//  Created by Jacob Banks on 9/12/15.
+//  Copyright (c) 2015 Jacob Banks. All rights reserved.
+//
+
+#import "JSQMessages.h"
