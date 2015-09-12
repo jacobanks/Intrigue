bca6ad6bce23bc7c72a85ab26c8b5cabfe80ae12
jacobanks <jacobanks@gmail.com>
2015-09-12 05:18:42 -0400

    Added SignupViewController and added login

--- Intrigue/SignupViewController.swift
+++ Intrigue/SignupViewController.swift
@@ -1,37 +1,38 @@
 //
-//  loginViewController.swift
-//  Intrigue
+//  SignupViewController.swift
+//  
 //
 //  Created by Jacob Banks on 9/12/15.
-//  Copyright (c) 2015 Jacob Banks. All rights reserved.
+//
 //
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class SignupViewController: UIViewController {
 
+    @IBOutlet var nameTextField: UITextField?
     @IBOutlet var emailTextField: UITextField?
     @IBOutlet var passwordTextField: UITextField?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func login(sender: AnyObject) {
-        let urlPath: String = "https://mhacks.on-aptible.com/api/auth/token"
+    @IBAction func createUser(sender: AnyObject) {
+        let urlPath: String = "https://mhacks.on-aptible.com/api/auth/register"
         var url: NSURL = NSURL(string: urlPath)!
         var request: NSMutableURLRequest = NSMutableURLRequest(URL: url)
         
         request.HTTPMethod = "POST"
-        var stringPost = "email=\(emailTextField!.text)&password=\(passwordTextField!.text)"
-
+        var stringPost = "email=\(emailTextField!.text)&password=\(passwordTextField!.text)&name=\(nameTextField!.text)"
+        
         let data = stringPost.dataUsingEncoding(NSUTF8StringEncoding)
         
         request.timeoutInterval = 60
@@ -42,12 +43,11 @@ class LoginViewController: UIViewController {
         
         NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler:{ (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
             var err: NSError
-            println(NSString(data: data, encoding: NSUTF8StringEncoding))
             var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
             println("AsSynchronous\(jsonResult)")
         })
     }
-
+    
     /*
     // MARK: - Navigation
 
