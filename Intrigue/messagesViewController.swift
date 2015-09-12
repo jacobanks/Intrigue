4b6dcd3ca363de0638443b6b0411562638627c4b
Jonathan Kingsley <me@jfkingsley.co.uk>
2015-09-12 14:02:03 -0400

    Added new system for messages

--- Intrigue/messagesViewController.swift
+++ Intrigue/messagesViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class MessagesViewController: JSQMessagesViewController {
     
     var userName = ""
+    var serviceID = 1
     var messages = [JSQMessage]()
     let incomingBubble = JSQMessagesBubbleImageFactory().incomingMessagesBubbleImageWithColor(UIColor(red: 10/255, green: 180/255, blue: 230/255, alpha: 1.0))
     let outgoingBubble = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImageWithColor(UIColor.lightGrayColor())
@@ -19,19 +20,28 @@ class MessagesViewController: JSQMessagesViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.userName = "iPhone"
-        for i in 1...10 {
-            var sender = (i%2 == 0) ? "Test User" : self.userName
-            var message = JSQMessage(senderId: sender, displayName: sender, text: "Text")
-            self.messages += [message]
-        }
+        self.senderId = "apple"
+        self.senderDisplayName = "Apple"
+        
+        self.collectionView.collectionViewLayout.springinessEnabled = true
+        
+        //Add actual messages
+        self.messages += [JSQMessage(senderId: "jon", displayName: "Jonathan", text: "I've always been fascinated with communication, using iMessage, Slack and Hipchat day-to-day.")]
+        self.messages += [JSQMessage(senderId: "jon", displayName: "Jonathan", text: "From communicating with clients to even building my own little chat servers.")]
+        self.messages += [JSQMessage(senderId: "jon", displayName: "Jonathan", text: "But I don't just use these personally.")]
+        
+        self.messages += [JSQMessage(senderId: "matt", displayName: "Matt", text: "Sure! lorem ipsum dolor sit amet bla bla bla bla bla")]
+        self.messages += [JSQMessage(senderId: "virgin", displayName: "Virgin Media", text: "Hey! We had a lot of fun working with Jon over autumn building Kipstr, and maybe bringing it to Apple Watch soon ;)")]
+        self.messages += [JSQMessage(senderId: "jon", displayName: "Jonathan", text: "Ahah, maybe. :P")]
+        
+        self.messages += [JSQMessage(senderId: "jon", displayName: "Jonathan", text: "So yeah, straight from the horses mouth. Thanks for your time! :)")]
+        
+        self.collectionView.reloadData()
         self.collectionView.reloadData()
         self.senderDisplayName = self.userName
         self.senderId = self.userName
         
         self.title = "Recipient name"
-        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
-        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -54,7 +64,7 @@ class MessagesViewController: JSQMessagesViewController {
     }
     
     override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
-        return nil
+        return JSQMessagesAvatarImageFactory.avatarImageWithUserInitials("MTS", backgroundColor: UIColor(white: 0.85, alpha: 1.0), textColor: UIColor(white: 0.60, alpha: 1.0), font: UIFont.systemFontOfSize(CGFloat(14.0)), diameter: UInt(kJSQMessagesCollectionViewAvatarSizeDefault))
     }
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
